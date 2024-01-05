f683a35b3d1312204ae80bf2a8fb660d9080b2f1
tripleh09 <kmaciejewski.09@wp.pl>
2024-01-05 11:58:59 +0100

    Dodanie kalkulatora pola koła

--- Pole_prostokata.c
+++ Pole_prostokata.c
@@ -1,6 +1,29 @@
 #include  <stdio.h>
+#include <math.h>
 
+float r = 0;
 float oblicz(float a, float b);
+
+
+float obliczPoleKola(float r){
+    return(M_PI*r*r);
+}
+
+void obsluzObliczaniePolaKola() {
+    printf("Podaj promien kola\t");
+    scanf("%f", &r);
+    printf("Pole kola wynosi:\t%f\n", obliczPoleKola(r));
+
+    return;
+}
+
+
+
+
+
+float oblicz(float a, float b) {
+	return(a*b);
+}
 
 float main() {
     float a = 0;
@@ -13,10 +36,9 @@ float main() {
 	scanf("%f", &b);
 
 
-	printf ("\n wynik:\t%f", oblicz(a,b));
+	printf ("\n wynik:\t%f\n", oblicz(a,b));
+
+	obsluzObliczaniePolaKola();
 	return 0;
 
-}
-float oblicz(float a, float b) {
-	return(a*b);
 }
