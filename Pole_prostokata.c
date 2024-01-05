75b91f658ba5cb99b62e57c0a98c457150e239c7
tripleh09 <kmaciejewski.09@wp.pl>
2024-01-05 12:21:34 +0100

    Dodanie menu

--- Pole_prostokata.c
+++ Pole_prostokata.c
@@ -1,15 +1,15 @@
-#include  <stdio.h>
+#include <stdio.h>
 #include <math.h>
 
-float r = 0;
-float oblicz(float a, float b);
-
+#define TRUE    1
 
 float obliczPoleKola(float r){
     return(M_PI*r*r);
 }
 
 void obsluzObliczaniePolaKola() {
+    float r;
+
     printf("Podaj promien kola\t");
     scanf("%f", &r);
     printf("Pole kola wynosi:\t%f\n", obliczPoleKola(r));
@@ -17,28 +17,36 @@ void obsluzObliczaniePolaKola() {
     return;
 }
 
-
-
-
-
-float oblicz(float a, float b) {
+float obliczPoleProstokata(float a, float b) {
 	return(a*b);
 }
 
-float main() {
-    float a = 0;
-    float b = 0;
-
+void obsluzObliczaniePolaProstokata() {
+    float a;
+    float b;
 
 	printf("Podaj wartosc a:\t ");
 	scanf("%f", &a);
 	printf("Podaj wartosc b:\t");
 	scanf("%f", &b);
 
+	printf ("\n wynik:\t%f\n", obliczPoleProstokata(a, b));
+}
 
-	printf ("\n wynik:\t%f\n", oblicz(a,b));
+int main(){
+    int select;
 
-	obsluzObliczaniePolaKola();
-	return 0;
+    while(TRUE) {
+        printf("\nOblicz Pole Prostokata - 1\nOblicz Pole ko³a - 2\nZakoñcz Dzia³anie Kalkulatora - 3\n");
+        scanf("%d", &select);
 
+        switch (select){
+            case 1: obsluzObliczaniePolaProstokata(); break;
+            case 2: obsluzObliczaniePolaKola(); break;
+            case 3: return 0;
+            default: printf("WprowadŸ poprawn¹ wartoœc\n");
+        }
+    }
+
+    return(0);
 }
